67c05f37a7ed2ed5ee28ece5492481236974a062
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2022-05-23 15:06:27 +0500

    Add MVC folder structure, update storyboard

--- Todo/Controller/TodoListViewController.swift
+++ Todo/Controller/TodoListViewController.swift
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TodoListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
